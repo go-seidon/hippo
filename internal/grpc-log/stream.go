cf6b209b97f2178b4d718dbdd7b0f0f41f7c38d9
KokoRaka <raka.suryaardi@gmail.com>
2022-10-01 20:52:43 +0700

    add message and stream log

--- internal/grpc-log/stream.go
+++ internal/grpc-log/stream.go
@@ -3,7 +3,10 @@ package grpc_log
 import (
 	"context"
 
+	"github.com/go-seidon/local/internal/logging"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/proto"
 )
 
 type ServerStream interface {
@@ -13,4 +16,47 @@ type ServerStream interface {
 	Context() context.Context
 	SendMsg(m interface{}) error
 	RecvMsg(m interface{}) error
+}
+
+type logServerStream struct {
+	grpc.ServerStream
+	logger logging.Logger
+}
+
+func (ss *logServerStream) SendMsg(m interface{}) error {
+	err := ss.ServerStream.SendMsg(m)
+	if err == nil {
+		var stream *messageMarshaller
+		msg, ok := m.(proto.Message)
+		if ok {
+			stream = NewMessage(msg)
+		}
+
+		logger := ss.logger.WithFields(map[string]interface{}{
+			"stream": stream,
+		})
+		logger.Info("send stream")
+	}
+	return err
+}
+
+func (ss *logServerStream) RecvMsg(m interface{}) error {
+	err := ss.ServerStream.RecvMsg(m)
+	if err == nil {
+		var stream *messageMarshaller
+		msg, ok := m.(proto.Message)
+		if ok {
+			stream = NewMessage(msg)
+		}
+
+		logger := ss.logger.WithFields(map[string]interface{}{
+			"stream": stream,
+		})
+		logger.Info("receive stream")
+	}
+	return err
+}
+
+func NewLogServerStream(ss grpc.ServerStream, logger logging.Logger) *logServerStream {
+	return &logServerStream{ss, logger}
 }
---
+++ internal/grpc-log/stream_test.go
@@ -0,0 +1,121 @@
+package grpc_log_test
+
+import (
+	"fmt"
+
+	grpc_log "github.com/go-seidon/local/internal/grpc-log"
+	mock_grpclog "github.com/go-seidon/local/internal/grpc-log/mock"
+	grpc_test "github.com/go-seidon/local/internal/grpc-test"
+	mock_logging "github.com/go-seidon/local/internal/logging/mock"
+	"github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Stream Package", func() {
+
+	Describe("Log Server Stream", func() {
+		var (
+			lss    grpc_log.ServerStream
+			ss     *mock_grpclog.MockServerStream
+			logger *mock_logging.MockLogger
+			msg    *grpc_test.TestData
+		)
+
+		BeforeEach(func() {
+			t := GinkgoT()
+			ctrl := gomock.NewController(t)
+			ss = mock_grpclog.NewMockServerStream(ctrl)
+			logger = mock_logging.NewMockLogger(ctrl)
+			lss = grpc_log.NewLogServerStream(ss, logger)
+			msg = &grpc_test.TestData{}
+		})
+
+		Context("SendMsg function", Label("unit"), func() {
+			When("error happened", func() {
+				It("should return error", func() {
+					expectErr := fmt.Errorf("network error")
+
+					ss.
+						EXPECT().
+						SendMsg(gomock.Eq(msg)).
+						Return(expectErr).
+						Times(1)
+
+					err := lss.SendMsg(msg)
+
+					Expect(err).To(Equal(expectErr))
+				})
+			})
+
+			When("error not happened", func() {
+				It("should return result", func() {
+					ss.
+						EXPECT().
+						SendMsg(gomock.Eq(msg)).
+						Return(nil).
+						Times(1)
+
+					logger.
+						EXPECT().
+						WithFields(gomock.Any()).
+						Return(logger).
+						Times(1)
+
+					logger.
+						EXPECT().
+						Info(gomock.Eq("send stream")).
+						Times(1)
+
+					err := lss.SendMsg(msg)
+
+					Expect(err).To(BeNil())
+				})
+			})
+		})
+
+		Context("RecvMsg function", Label("unit"), func() {
+			When("error happened", func() {
+				It("should return error", func() {
+					expectErr := fmt.Errorf("network error")
+
+					ss.
+						EXPECT().
+						RecvMsg(gomock.Eq(msg)).
+						Return(expectErr).
+						Times(1)
+
+					err := lss.RecvMsg(msg)
+
+					Expect(err).To(Equal(expectErr))
+				})
+			})
+
+			When("error not happened", func() {
+				It("should return result", func() {
+					ss.
+						EXPECT().
+						RecvMsg(gomock.Eq(msg)).
+						Return(nil).
+						Times(1)
+
+					logger.
+						EXPECT().
+						WithFields(gomock.Any()).
+						Return(logger).
+						Times(1)
+
+					logger.
+						EXPECT().
+						Info(gomock.Eq("receive stream")).
+						Times(1)
+
+					err := lss.RecvMsg(msg)
+
+					Expect(err).To(BeNil())
+				})
+			})
+		})
+	})
+
+})
